43cf15ac68ff30df35ac67106c3852636d3e313e
Yanir <6667420+yanirta@users.noreply.github.com>
2019-02-12 13:09:47 +0200

    - Adding env - Updating the badge in readme to point to the recent version. - Version up

--- src/main/java/com/applitools/Commands/Version.java
+++ src/main/java/com/applitools/Commands/Version.java
@@ -4,7 +4,7 @@ import com.beust.jcommander.Parameters;
 
 @Parameters(commandDescription = "Print the version and more details about the tool")
 public class Version implements Command {
-    private String version = "1.0.7.1";
+    private String version = "1.0.7.2";
 
     @Override
     public void run() throws Exception {
