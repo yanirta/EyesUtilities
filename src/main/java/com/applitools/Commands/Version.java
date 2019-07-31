c3904c36238dc209b0f5362210aa861c2fe3088f
Yanir <6667420+yanirta@users.noreply.github.com>
2019-07-31 18:00:37 +0300

    turns out 1.0.7.3 was already released, so version up.

--- src/main/java/com/applitools/Commands/Version.java
+++ src/main/java/com/applitools/Commands/Version.java
@@ -4,7 +4,7 @@ import com.beust.jcommander.Parameters;
 
 @Parameters(commandDescription = "Print the version and more details about the tool")
 public class Version implements Command {
-    private String version = "1.0.7.3";
+    private String version = "1.0.7.4";
 
     public void run() throws Exception {
         System.out.printf(
